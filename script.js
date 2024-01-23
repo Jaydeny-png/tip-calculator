9d556570b2ac667eb58f18095027c3ead7ede3db
Jaydeny-png <jyau1020@gmail.com>
2024-01-23 10:43:32 -0500

    done

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-function calculateAndDisplay()
-{
+function calculateAndDisplay() {
     let bill = parseFloat(document.getElementById("totalBill").value)
-    let numOfPpl = parseInt(document.getElementById('numofPeople').value)
+    let numOfPpl = parseInt(document.getElementById('numOfPeople').value)
     let serviceQuality = document.getElementById('serviceQuality').value
 
     let tip = calculateTip(bill, serviceQuality)
-    let totalBIll = calcualteTotalBill(bill, tip)
-    let amtPerPers = calculateAmtperPers(bill, numOfPpl)
+    let totalBill = calculateTotalBill(bill, tip)
+    let amtPerPers = calculateAmtPerPers(bill, numOfPpl)
 
-     // Display the results in the HTML document
-  document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
-  document.getElementById('totalBillResult').innerText =
-    'Total Bill: $' + totalBill.toFixed(2)
-  document.getElementById('amtPerPersonResult').innerText =
-    'Amount per person: $' + amtPerPers.toFixed(2)
+    // Display the results in the HTML document
+    document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
+    document.getElementById('totalBillResult').innerText =
+        'Total Bill: $' + totalBill.toFixed(2)
+    document.getElementById('amtPerPersonResult').innerText =
+        'Amount per person: $' + amtPerPers.toFixed(2)
 
 }
 
 
 
+//Helper Function to Calculate Tip based on the bill and service Quality
+function calculateTip(bill, quality){
+    if(quality === 'Great'){
+        return bill * .2
+    } else if (quality === 'Good'){
+        return bill * .15
+    } else if(quality === 'Poor'){
+        return bill * .10
+    }else {
+        return 0
+            //alert type smth later
+    }
+}
 
+//Helper Function to Calculate Tip based on the bill and service Quality
+function calculateTotalBill(bill, tip){
+    return bill + tip
+}
 
-
-
-
+function calculateAmtPerPers(totalBill, numOfPpl){
+    return totalBill / numOfPpl
+}
 
 
 
